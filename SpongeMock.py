f96561999ac0b0e16a4063f1d75b11f5c142a776
Michael Blackmer <xmcbii@gmail.com>
2020-12-01 12:30:53 -0600

    Files uploaded
    Added the primary code file for the bot and the image used by the bot to send in the Discord.

---
+++ SpongeMock.py
@@ -0,0 +1,42 @@
+# Mock Text Discord Bot
+
+
+import discord
+
+TOKEN = 'NzA1OTQyNjI2NDU2NTY3ODI5.XqzCTQ.yetPaP2VBHW6cnkPx6Sl1oshgIU'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!mock'):
+        channel = message.channel
+        newmsg = message.content
+        listmsg = newmsg.split()
+        listmsg.remove("!mock")        
+        def lst2str(s):  
+            str1 = " "  
+            return (str1.join(listmsg)) 
+        msgcontent = lst2str(listmsg) 
+
+        def mock(st):
+            final = []
+            for index, c in enumerate(st):
+                if index % 2 == 0 & c.isalpha():
+                    final.append(c.lower())
+                else:
+                    final.append(c.upper())
+            return ''.join(final)
+
+        await channel.send(file=discord.File('sbob.png'))
+        await channel.send("***" + mock(msgcontent) + "***")
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
